76cd58ae93267d8af1896949e789e3380ea162e0
Michał Skolak <76774237+Skolaczk@users.noreply.github.com>
2024-01-19 17:18:47 +0100

    Feature/NS-2-configure-tools (#4)
    * Install needed packages
    
    * Configure tools

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  endOfLine: 'auto',
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  trailingComma: 'es5',
+  tabWidth: 2,
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { PropsWithChildren } from "react";
+import { PropsWithChildren } from 'react';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ["latin"] });
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Next starter",
-  description: "Next starter",
+  title: 'Next starter',
+  description: 'Next starter',
 };
 
 const RootLayout = ({ children }: PropsWithChildren) => {
