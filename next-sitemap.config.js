a6107bef2a668f55a27495aab78ec99e57628e2e
Michał <skolakmichal1@gmail.com>
2024-01-21 14:36:27 +0100

    Add og data

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_SITE_URL,
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || '',
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [{ userAgent: '*', allow: '/' }],
--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,5 +1,6 @@
 export const siteConfig = {
   title: 'Next.js Starter',
   description: 'Next.js Starter',
-  url: process.env.NEXT_PUBLIC_SITE_URL,
+  keywords: ['Next.js', 'Tailwindcss', 'TypeScript'],
+  url: process.env.NEXT_PUBLIC_SITE_URL || '',
 };
