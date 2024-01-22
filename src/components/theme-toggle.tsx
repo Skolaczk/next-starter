6bae2bc44fcf009ac3ff20957c9a7f2d02ce4b25
Michał <skolakmichal1@gmail.com>
2024-01-22 17:21:10 +0100

    Add shadcn/ui

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -3,17 +3,19 @@
 import { useTheme } from 'next-themes';
 
 import { Icons } from '@/components/icons';
+import { Button } from '@/components/ui/button';
 
 export const ThemeToggle = () => {
   const { theme, setTheme } = useTheme();
 
   return (
-    <button
-      className="flex gap-2 bg-neutral-100 p-3 dark:bg-neutral-800"
+    <Button
+      variant="secondary"
+      size="icon"
       onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
     >
       <Icons.sun className="hidden dark:block" />
       <Icons.moon className="dark:hidden" />
-    </button>
+    </Button>
   );
 };
