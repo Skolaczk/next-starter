55158dd4cf5ce55684404127dc28f11fd6369cdd
Michał <skolakmichal1@gmail.com>
2024-02-05 18:02:11 +0100

    feat: add prisma

--- src/app/api/auth/[...nextauth]/auth-options.ts
+++ src/app/api/auth/[...nextauth]/auth-options.ts
@@ -1,9 +1,12 @@
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import type { NextAuthOptions } from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
 
 import { env } from '@/env.mjs';
+import prisma from '@/lib/prisma';
 
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GitHubProvider({
       clientId: env.NEXT_PUBLIC_GITHUB_ID || '',
---
+++ src/lib/prisma.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from '@prisma/client';
+
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
+const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV === 'development') global.prisma = prisma;
+
+export default prisma;
