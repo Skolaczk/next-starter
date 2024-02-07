08a02f997aaaa8b6db9a2d90dcae08a0c70e6985
Michał <skolakmichal1@gmail.com>
2024-02-07 12:31:07 +0100

    feat: update content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,17 @@ import { Button } from '@/components/ui/button';
 const Home = () => {
   return (
     <section className="container mt-10 flex flex-col items-center gap-3 text-center md:absolute md:left-1/2 md:top-1/2 md:mt-0 md:-translate-x-1/2 md:-translate-y-1/2">
-      <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-        Next.js + TypeScript + TailwindCSS + Shadcn/ui
+      <h1 className="mb-1 font-mono text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+        Next.js starter template
       </h1>
       <p className="text-muted-foreground max-w-2xl">
-        Kickstart your project with a polished foundation. Effortlessly
-        integrate professionally designed components. Easily customizable. Open
-        Source. Your journey to a stunning web application begins here.
+        A Next.js starter template, packed with features like TypeScript,
+        Tailwind CSS, Next-auth, Eslint, testing tools and more. Jumpstart your
+        project with efficiency and style.
       </p>
+      <div className="mt-1">
+        <HeroForm />
+      </div>
       <div className="mt-2 flex gap-4">
         <Button asChild>
           <a
@@ -27,9 +30,6 @@ const Home = () => {
             <Icons.github className="mr-2 size-4" /> Github
           </a>
         </Button>
-      </div>
-      <div className="mt-5">
-        <HeroForm />
       </div>
     </section>
   );
