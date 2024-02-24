f0c8530baa258d91630146987ffd16ed0050e2dc
Michał <skolakmichal1@gmail.com>
2024-02-24 10:44:21 +0100

    fix: fix fonts display

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -3,11 +3,13 @@ import { Inter, JetBrains_Mono } from 'next/font/google';
 const fontSans = Inter({
   subsets: ['latin'],
   variable: '--font-sans',
+  display: 'swap',
 });
 
 const fontMono = JetBrains_Mono({
   subsets: ['latin'],
   variable: '--font-mono',
+  display: 'swap',
 });
 
 export const fonts = [fontSans.variable, fontMono.variable];
