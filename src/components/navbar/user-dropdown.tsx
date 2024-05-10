67974bdfbf6b3ecd2f62302086bb1d84ad68bfca
Michał <skolakmichal1@gmail.com>
2024-05-10 17:07:13 +0200

    feat: create UI for subscription functionality

--- src/components/navbar/user-dropdown.tsx
+++ src/components/navbar/user-dropdown.tsx
@@ -5,26 +5,47 @@ import { Session } from 'next-auth';
 import { signOut } from 'next-auth/react';
 
 import { Icons } from '@/components/icons';
+import { Button } from '@/components/ui/button';
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 
-export const UserDropdown = ({ session }: { session: Session }) => {
+export const UserDropdown = ({ session: { user } }: { session: Session }) => {
+  const isPro = false;
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
         <Image
           className="overflow-hidden rounded-full"
-          src={`${session.user?.image}`}
-          alt={`${session.user?.name}`}
+          src={`${user?.image}`}
+          alt={`${user?.name}`}
           width={32}
           height={32}
         />
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <div className="flex flex-col items-center justify-center p-2">
+          <Image
+            className="overflow-hidden rounded-full"
+            src={`${user?.image}`}
+            alt={`${user?.name}`}
+            width={100}
+            height={100}
+          />
+          <h2 className="py-2 text-lg font-bold">{user?.name}</h2>
+          <Button disabled={isPro} className="w-48">
+            {isPro ? 'You are pro!' : 'Upgrade to Pro'}
+          </Button>
+        </div>
+        <DropdownMenuSeparator />
         <DropdownMenuItem onClick={() => signOut()}>
           <Icons.logOut className="mr-2 size-4" /> <span>Log out</span>
         </DropdownMenuItem>
