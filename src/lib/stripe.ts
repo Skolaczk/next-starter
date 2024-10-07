3a3c0db8d7b1b113b32e11f8514861e9069a9106
Michał Skolak <skolakmichal1@gmail.com>
2024-10-07 18:00:56 +0200

    fix: fix stripe version error

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -3,5 +3,5 @@ import Stripe from 'stripe';
 import { env } from '@/env.mjs';
 
 export const stripeServer = new Stripe(env.STRIPE_SECRET_KEY, {
-  apiVersion: '2024-06-20',
+  apiVersion: '2024-09-30.acacia',
 });
