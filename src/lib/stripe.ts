c945df76778edca22a07b3af47fa235be9ec47bf
Michał Skolak <skolakmichal1@gmail.com>
2024-11-11 21:00:27 +0100

    fix: fix types error

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -3,5 +3,5 @@ import Stripe from 'stripe';
 import { env } from '@/env.mjs';
 
 export const stripeServer = new Stripe(env.STRIPE_SECRET_KEY, {
-  apiVersion: '2024-09-30.acacia',
+  apiVersion: '2024-10-28.acacia',
 });
