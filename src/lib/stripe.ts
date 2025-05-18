928d925bf152c7c0d35c7631b405bf6954deaa33
Michał Skolak <skolakmichal1@gmail.com>
2025-05-18 11:12:11 +0200

    refactor: final touches

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -3,5 +3,5 @@ import Stripe from 'stripe';
 import { env } from '@/env.mjs';
 
 export const stripeServer = new Stripe(env.STRIPE_SECRET_KEY, {
-  apiVersion: '2024-12-18.acacia',
+  apiVersion: '2025-02-24.acacia',
 });
