ae238acdf179c14717a2a7af3d9fae0685194495
Michał Skolak <skolakmichal1@gmail.com>
2025-06-14 16:41:05 +0200

    fix: fix jest error

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,18 @@ const createJestConfig = nextJest({
 const config = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   testEnvironment: "jest-environment-jsdom",
+  preset: "ts-jest",
   testPathIgnorePatterns: ["<rootDir>/src/__tests__/e2e"],
+  transform: {
+    "^.+\\.jsx?$": "babel-jest",
+    "^.+\\.tsx?$": "ts-jest",
+  },
 };
+const asyncConfig = createJestConfig(config);
 
-module.exports = createJestConfig(config);
+module.exports = async () => {
+  const config = await asyncConfig();
+  config.transformIgnorePatterns = [];
+
+  return config;
+};
