6dbfb16e7aa688ed82e2ead712c4d366025216e2
Michał Skolak <skolakmichal1@gmail.com>
2026-08-29 12:34:05 +0200

    feat: update stripe version

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -3,5 +3,5 @@ import Stripe from "stripe";
 import { env } from "@/env.mjs";
 
 export const stripeServer = new Stripe(env.STRIPE_SECRET_KEY, {
-  apiVersion: "2026-07-29.dahlia",
+  apiVersion: "2026-08-26.dahlia",
 });
